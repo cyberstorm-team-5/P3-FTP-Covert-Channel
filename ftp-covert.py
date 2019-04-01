21b963e298c022ca6169863d25f25dda733b9a45
Andrew Maurice <andromulin@gmail.com>
2019-04-01 08:04:36 -0500

    added initial files
    assignment writeup and formatted python file uploaded

---
+++ ftp-covert.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2.7
+################################################################
+# Authors: Team Chinese (Lane Arnold, Christopher Boquet,
+# 	   Christopher Bouton, Darrell Durousseaux, Clay Fonseca,
+#	   Rebecca Grantham, Andrew Maurice)
+# Class: CSC 442
+# Date: 4-5-2019
+# Github Repo: https://github.com/cyberstorm-team-5/P3-FTP-Covert-Channel-Team-Chinese
+# Description: Program 3: FTP (storage) Covert Channel
+#              The Python code will extract a covert message from the
+#              file permissions of a FTP server. It first fetches the file
+#              listing and (based on the METHOD variable at the top of the
+#              code), will either use the last 7 or all 10 bits of the file
+#              permissions to generate and output the covert message.
+################################################################
+
+import sys
+###############################################################
+
+
+
+
+
+########################MAIN###################################
+
+
+
